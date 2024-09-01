0f78125489712ce7e485759d404d81c2b6c5328e
树下的码农 <94507028+hanxuyyds@users.noreply.github.com>
2024-09-01 20:16:48 +0800

    Add files via upload

---
+++ SingleUserMultiLoginRestriction/src/test/java/com/example/singleusermultiloginrestriction/SingleUserMultiLoginRestrictionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.singleusermultiloginrestriction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleUserMultiLoginRestrictionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
